8d142326074093b9f7d7aae6d70e63d0605315d9
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-01-26 22:19:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,111 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import factorial
+from scipy.special import lpmv
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Harmoniques Sphériques")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def slider():
+    n = st.slider('l',1,5,3)
+    return n
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def slider2():
+    n = st.slider('r',1,5,3)
+    return n
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def slider3():
+    n = st.slider('Complexe/Réel',0,1,1)
+    return n
+
+N1 = slider()
+N2 = slider2()
+N3 = slider3()
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+theta = np.linspace(0, np.pi, 100)
+phi = np.linspace(0, 2*np.pi, 100)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+theta, phi = np.meshgrid(theta, phi)
+
+xyz = np.array([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
+
+def SimuSphHarm (ax, l, m, ri):
+
+    
+            
+    
+    def shr (m, l, phi, theta):
+ #Définition des HS réelles        
+        A = (2*l) / (4*np.pi)
+        b = l - np.abs(m)
+        B = factorial(b)
+        c = l + np.abs(m)
+        C = factorial(c)
+        D = lpmv(m, l, np.cos(theta))
+        E = np.cos( m * phi )
+        H = (-1)**m
+        G = H*(np.sqrt(2) * np.sqrt( A * (B/C)) * D * E)
+        return G
+#Définition des HS imaginaires  
+    def shc (m, l, phi, theta):
+         
+        A = (2*l) / (4*np.pi)
+        b = l - np.abs(m)
+        B = factorial(b)
+        c = l + np.abs(m)
+        C = factorial(c)
+        D = lpmv(m, l, np.cos(theta))
+        E = np.sin( m * phi )
+        H = (-1)**m
+        G =  H*(np.sqrt(2) * np.sqrt( A * (B/C)) * D * E)
+        return G
+ #Switch pour obtenir soit la partie réelle ou imaginaire 1 = réel 0  = imaginaire  
+    def realimg(ri):
+        if ri == 1:
+            R = shr (m,l,phi,theta)
+        elif ri == 0:
+            R = shc (m,l,phi,theta)
+        return R
+            
+    SH = realimg(ri)    
+  
+    HSx, HSy, HSz = np.abs(SH)*xyz
+    
+#On colore la surface en fonction du signe de HS, vert = positif et rouge = négatif
+    
+    cmap = plt.cm.ScalarMappable(cmap=plt.get_cmap('RdYlGn'))
+    cmap.set_clim(-0.35, 0.35)
+    
+    ax.plot_surface(HSx, HSy, HSz,
+                    facecolors=cmap.to_rgba(SH),
+                    rstride=1, cstride=1)
+    
+#On trace les axes x, y et z     
+    ax_lim = 0.5
+    ax.plot([-ax_lim, ax_lim], [0,0], [0,0], c='0.5', lw=1, zorder=10)
+    ax.plot([0,0], [-ax_lim, ax_lim], [0,0], c='0.5', lw=1, zorder=10)
+    ax.plot([0,0], [0,0], [-ax_lim, ax_lim], c='0.5', lw=1, zorder=10)
+    
+#Affichafe du titre et suppression du cadre
+    
+    # ax.set_title("$f_{z^3}$")
+    ax_lim = 0.5
+    ax.set_xlim(-ax_lim, ax_lim)
+    ax.set_ylim(-ax_lim, ax_lim)
+    ax.set_zlim(-ax_lim, ax_lim)
+    ax.set_xlabel('X ')
+    ax.set_ylabel('Y ')
+    ax.set_zlabel('Z ')
+    ax.axis('off')
+    
+    
+fig = plt.figure(figsize=plt.figaspect(1))
+ax = fig.add_subplot(projection='3d')
+l, m, ri  = N1 , N2 , N3
+SimuSphHarm(ax, l, m, ri)    
+
+st.pyplot(plt)
